fb615258bc454d198f3882d3ab0b2915d8f5ab36
Shasank Kumar <ssharma@amps.com>
2026-07-15 19:34:02 +0530

    vite config fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import App from "@/App";
-import "@index.css";
+import "@/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 6070,
+    open: true,
+  },
 });
